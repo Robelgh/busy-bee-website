6afbb6c3fe1667bec57a95b4d5b24ca50dce3444
Robelgh <robelghw123@gmail.com>
2023-07-02 23:09:33 +0300

    v1.1

--- pb/src/App.js
+++ pb/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./compenent/Navigation";
+import { Header } from "./compenent/Header";
+import { Features } from "./compenent/Features";
+import { About } from "./compenent/About";
+import { Services } from "./compenent/Services";
+import { Testimonials } from "./compenent/Testimonial";
+import { Team } from "./compenent/Team";
+// import { Contact } from "./compenent/Contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
 
-function App() {
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      {/* <Contact data={landingPageData.Contact} /> */}
     </div>
   );
-}
+};
 
 export default App;
